e528e4e5a8b5606b3376351b79bca82e5ab9f853
stanleyjulien <tijesus151@gmail.com>
2017-10-09 15:30:42 -0500

    add model classes

---
+++ src/main/java/edu/mum/mpp/model/Block.java
@@ -0,0 +1,34 @@
+package edu.mum.mpp.model;
+
+public class Block extends AbstractModel {
+    private long id;
+    private String name;
+    private String location;
+
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
