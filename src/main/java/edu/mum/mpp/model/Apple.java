061caa41725acf1977c6ce99cc518c53d4b4d280
jedaademola <lukman.arogundade@gmail.com>
2017-10-17 13:30:43 -0500

    updates

---
+++ src/main/java/edu/mum/mpp/model/Apple.java
@@ -0,0 +1,22 @@
+package edu.mum.mpp.model;
+
+public class Apple {
+    private String color;
+    private int weight;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
