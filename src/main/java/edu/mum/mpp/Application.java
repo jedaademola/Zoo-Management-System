ae21ef76a9aab74c565844e5a6d1727547e113ec
Lukman Arogundade <jedaademola@users.noreply.github.com>
2018-05-27 09:14:14 -0500

    add swagger UI

--- src/main/java/edu/mum/mpp/Application.java
+++ src/main/java/edu/mum/mpp/Application.java
@@ -2,8 +2,15 @@ package edu.mum.mpp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {//extends SpringBootServletInitializer {
 
     //@Override
@@ -13,5 +20,14 @@ public class Application {//extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("edu.mum.mpp.controller  "))  // // Generate API of EndPoints which is available inside defined package
+                .paths(PathSelectors.any())   // for all EndPoints
+                .build();             // create object
     }
 }
